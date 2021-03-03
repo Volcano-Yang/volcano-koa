4f1a27a0375589ff15b25f6f7ce0d1524ca1b40f
Volcano-Yang <648941183@qq.com>
2021-03-03 10:41:07 +0800

    feat: 解决全局异常中间不打印开发中的错误信息问题

--- middlewares/errorHander.js
+++ middlewares/errorHander.js
@@ -1,28 +1,39 @@
-const {HttpException} = require('../core/http-exception')
+const { HttpException } = require("../core/http-exception");
 
-const catchError = async (ctx, next)=>{
+const catchError = async (ctx, next) => {
     try {
-        await next()
+        await next();
     } catch (error) {
-        const isHttpException = error instanceof HttpException
+        const isHttpException = error instanceof HttpException;
+        const isDev = global.config.environment === "dev";
+
+        /**
+         * 1. 开发过程中我们经常会遇到一些奇怪的错误，这些是我们是我们之前没有想到的错误情况，都是非HttpException的。
+         * 2. 在开发环境中，遇到错误不抛出，只是返回500，没有错误报告的打印，这样会不便于我们的开发。
+         */
         
-        if(isHttpException){
-            ctx.body = {
-                msg:error.msg,
-                error_code:error.errorCode,
-                request:`${ctx.method} ${ctx.path}`
-            }
-            ctx.status = error.code
+        if (isDev && !isHttpException) {
+            throw error;
         }
-        else{
+
+        if (isHttpException) {
+            // 意料之中的情况
             ctx.body = {
-                msg: 'we made a mistake O(∩_∩)O~~',
+                msg: error.msg,
+                error_code: error.errorCode,
+                request: `${ctx.method} ${ctx.path}`,
+            };
+            ctx.status = error.code;
+        } else {
+            // 意料之外的情况
+            ctx.body = {
+                msg: "we made a mistake O(∩_∩)O~~",
                 error_code: 999,
-                request:`${ctx.method} ${ctx.path}`
-            }
-            ctx.status = 500
+                request: `${ctx.method} ${ctx.path}`,
+            };
+            ctx.status = 500;
         }
     }
-}
+};
 
-module.exports = catchError
+module.exports = catchError;
